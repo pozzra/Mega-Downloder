7e26ccfd678b353c257ff62b0adae7da8035401e
pozzra <k.amra999+1@yandex.com>
2025-12-19 20:35:52 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,7 @@ export const Footer = () => {
           
           <div className="flex flex-col sm:flex-row gap-4">
              <a 
-              href="https://mega.nz/file/Op4DWIQT#JCvKwaftmevEbkaFdx8pK8KVvp9gc8hPNzgHTLXLRlg"
+              href="https://mega.nz/file/K9JSCRQC#SzyAdqdZG5FxJXuEhvTLPfvDdPc1RFVqgTXNJiYdqS8"
               target="_blank"
               rel="noopener noreferrer"
               className="px-6 py-3 bg-gray-800 hover:bg-gray-700 text-white rounded-lg transition-colors flex items-center justify-center gap-2 border border-gray-700 cursor-pointer"
